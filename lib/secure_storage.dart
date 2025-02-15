402d83afe8e1b4619a98253f8ceca465ad58dcb9
unknown <aghniafikri1205@gmail.com>
2025-02-15 10:11:47 +0700

    Fix secure storage for web and mobile

--- lib/secure_storage.dart
+++ lib/secure_storage.dart
@@ -1,17 +1,51 @@
+import 'package:flutter/foundation.dart'; // Untuk kIsWeb
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-final storage = FlutterSecureStorage();
+class SecureStorageService {
+  static final SecureStorageService _instance =
+      SecureStorageService._internal();
 
-Future<void> saveCredentials(String username, String password) async {
-  await storage.write(key: 'username', value: username);
-  await storage.write(key: 'password', value: password);
-}
-
-Future<Map<String, String>?> getCredentials() async {
-  final username = await storage.read(key: 'username');
-  final password = await storage.read(key: 'password');
-  if (username != null && password != null) {
-    return {'username': username, 'password': password};
+  factory SecureStorageService() {
+    return _instance;
   }
-  return null;
+
+  SecureStorageService._internal();
+
+  late final dynamic _storage;
+
+  Future<void> init() async {
+    if (kIsWeb) {
+      _storage = await SharedPreferences.getInstance();
+    } else {
+      _storage = const FlutterSecureStorage();
+    }
+  }
+
+  Future<void> saveCredentials(String username, String password) async {
+    if (kIsWeb) {
+      await _storage.setString('username', username);
+      await _storage.setString('password', password);
+    } else {
+      await _storage.write(key: 'username', value: username);
+      await _storage.write(key: 'password', value: password);
+    }
+  }
+
+  Future<Map<String, String>?> getCredentials() async {
+    if (kIsWeb) {
+      final username = _storage.getString('username');
+      final password = _storage.getString('password');
+      if (username != null && password != null) {
+        return {'username': username, 'password': password};
+      }
+    } else {
+      final username = await _storage.read(key: 'username');
+      final password = await _storage.read(key: 'password');
+      if (username != null && password != null) {
+        return {'username': username, 'password': password};
+      }
+    }
+    return null;
+  }
 }
